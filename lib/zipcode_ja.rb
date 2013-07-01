66b254b2b98ccb9c2fbbdb394f836f5f9b8f7488
mizoryu <suzunatsu@yahoo.com>
2013-07-02 06:37:03 +0900

    fix ZipcodeJa#find

--- lib/zipcode_ja.rb
+++ lib/zipcode_ja.rb
@@ -3,35 +3,18 @@ require "zipcode_ja/string"
 require "csv"
 
 module ZipcodeJa
-  @_zipcodes = {}
-  @_cache_length = 20
-  
+  @here = File.dirname(__FILE__)
   class << self
     def find(zipcode)
       zipcode =~ /^[0-9]{7}$/ or return
 
-      cc = zipcode[0, 2].to_sym
-      if !@_zipcodes[cc]
-        zip_path = File.join(File.dirname(__FILE__), "../data/zipcode-#{cc}xxxxx.csv")
-        FileTest.file?(zip_path) or return
+      ccc = zipcode[0, 3]
+      zip_path = File.join(@here, "../data/zipcode/#{ccc}xxxx.csv")
+      FileTest.file?(zip_path) or return
 
-        @_zipcodes[cc] = {}
-        CSV.foreach(File.open(zip_path)) do |row|
-          @_zipcodes[cc][row[2]] = row
-        end
-
-        clear_cache if @_zipcodes.length > @_cache_length
+      CSV.foreach(File.open(zip_path)) do |row|
+        return row if zipcode == row[2]
       end
-
-      @_zipcodes[cc][zipcode]
-    end
-
-    private
-
-    def clear_cache
-      cc = @_zipcodes.first[0]
-      @_zipcodes[cc].clear
-      @_zipcodes.delete(cc)
     end
   end
 end
