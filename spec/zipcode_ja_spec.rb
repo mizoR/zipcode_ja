641ae9d390553c58c056f055403e6bc58b58b057
mizoR <suzunatsu@yahoo.com>
2013-07-02 09:08:06 +0900

    都道府県データ更新
    dotcloudの提供するCSVのフォーマットにおいて、読みが全角カナから半角カナになっているので注意

--- spec/zipcode_ja_spec.rb
+++ spec/zipcode_ja_spec.rb
@@ -7,9 +7,9 @@ describe ZipcodeJa do
       "04606",
       "98607",
       "9860742",
-      "ミヤギケン",
-      "モトヨシグンミナミサンリクチョウ",
-      "シヅガワモトハマチョウ",
+      "ﾐﾔｷﾞｹﾝ",
+      "ﾓﾄﾖｼｸﾞﾝﾐﾅﾐｻﾝﾘｸﾁｮｳ",
+      "ｼﾂﾞｶﾞﾜﾓﾄﾊﾏﾁｮｳ",
       "宮城県",
       "本吉郡南三陸町",
       "志津川本浜町",
@@ -24,17 +24,5 @@ describe ZipcodeJa do
 
   it 'notfound region from zipcode' do
     expect('9999999'.to_region).to eq(nil)
-  end
-
-  it do
-    ZipcodeJa.instance_eval { @_cache_length = 5 }
-    expect {
-      10.times do |i|
-        ("%0d" % i + '00000').to_region
-      end
-      10.times do |i|
-        ("%0d" % i + '00001').to_region
-      end
-    }.to_not raise_error
   end
 end
